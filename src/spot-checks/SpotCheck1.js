adcffca1aba2c18a7a0025500b67970eb7c1bfe3
MatanNahum <matan.nahum95@gmail.com>
2022-11-12 13:23:14 +0200

    spot check 1

--- src/spot-checks/SpotCheck1.js
+++ src/spot-checks/SpotCheck1.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function SpotCheck1() {
-  // your code here
-  
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const addPage = () => setPageNumber(pageNumber + 1);
+  const backPage = () => setPageNumber(pageNumber - 1);
 
   return (
     <div className="ex-space">
       <h4 className="ex-title">Spot Check 1</h4>
       <div className="exercise" id="sc-1">
-        {/* your code here... */}
-
+        {pageNumber}
+        <button onClick={addPage} className="next">
+          next
+        </button>
+        <button onClick={backPage} className="back">
+          back
+        </button>
       </div>
     </div>
-  )
+  );
 }
